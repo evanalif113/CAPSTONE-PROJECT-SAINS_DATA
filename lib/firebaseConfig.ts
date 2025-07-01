bbf5ab2f586ad971d67e3a750d486a0d78fffefc
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-07-01 17:03:42 +0700

    refactor: update Firebase configuration to use environment variables for sensitive data

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -29,15 +29,14 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD14wZkvP46yP3jQAwzUBOSh9kf8m-7vwg",
-  authDomain: "kumbung-sense.firebaseapp.com",
-  databaseURL:
-    "https://kumbung-sense-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "kumbung-sense",
-  storageBucket: "kumbung-sense.firebasestorage.app",
-  messagingSenderId: "845687946504",
-  appId: "1:845687946504:web:3540585e9d68d4ec2d40bd",
-  measurementId: "G-F9H5CQQ24V",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
